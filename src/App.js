74838720d29f2a625fd049e7a7755b4b8ac414d4
lenon nascimento <lenoninformatica@gmail.com>
2022-10-07 17:09:22 -0300

    Requisito 3 feito

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import TelaPrincipal from './components/TelaPrincipal';
+import CarrinhoCompras from './components/CarrinhoCompras';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Route exact path="/" component={ TelaPrincipal } />
+        <Route path="/carrinhocompras" component={ CarrinhoCompras } />
       </BrowserRouter>
     );
   }
---
+++ src/components/CarrinhoCompras.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CarrinhoCompras extends React.Component {
+  render() {
+    return (
+      <div
+        data-testid="shopping-cart-empty-message"
+      >
+        Seu carrinho está vazio
+      </div>
+    );
+  }
+}
+export default CarrinhoCompras;
