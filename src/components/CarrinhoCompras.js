211cc46f52caff04f4ad9b573f64b99dd10b70b8
lenon nascimento <lenoninformatica@gmail.com>
2022-10-10 17:36:12 -0300

    Requisito 10 em desenvolvimento

--- src/components/CarrinhoCompras.js
+++ src/components/CarrinhoCompras.js
@@ -10,6 +10,26 @@ class CarrinhoCompras extends React.Component {
     return localArr;
   };
 
+  /*   handleMains = ({ target }) => {
+    const { innerText, name } = target;
+
+    if (innerText === '+') {
+      const data = this.recoveryProducts();
+      const dataFilter = data.reduce((item) => (
+
+      ));
+      // localStorage.setItem('cartItems');
+      /* dataFilter.quantity += 1; */
+  /*       console.log(dataFilter);
+      console.log(typeof dataFilter);
+      console.log(typeof dataFilter.quantity);
+    }
+  };  */
+
+  /* handleMore = () => {
+
+  }; */
+
   render() {
     const arr = this.recoveryProducts();
     return (
@@ -23,8 +43,33 @@ class CarrinhoCompras extends React.Component {
           </p>)
           : arr.map((produto, index) => (
             <div key={ index }>
+              <button
+                data-testid="remove-product"
+                type="button"
+                onClick=""
+              >
+                x
+              </button>
               <div data-testid="shopping-cart-product-name">{produto.title}</div>
+              <button
+                data-testid="product-increase-quantity"
+                type="button"
+                onClick=""
+              >
+                -
+              </button>
+
               <div data-testid="shopping-cart-product-quantity">{produto.quantity}</div>
+
+              <button
+                name={ produto.title }
+                data-testid="product-decrease-quantity"
+                type="button"
+                onClick={ this.handleMains }
+              >
+                +
+              </button>
+
               <div>{produto.price}</div>
               <img src={ produto.img } alt={ produto.title } />
             </div>
